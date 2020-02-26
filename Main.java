7157521e22ca55ab9266dca00a3c709632ffcb8e
Uğur Berkecan <ugurberkecanunlu@gmail.com>
2020-02-26 14:58:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main { 
+    public static void main(String[] args) 
+    { 
+        int []arra = new int[5];
+        // Declaring 1-D array with size 4 
+        int arr[] = { 10, 20, 30, 40 }; 
+  
+        // Printing elements of an array using 
+        // foreach loop 
+        for (int x : arra) 
+            System.out.println(x); 
+    } 
+} 
